a3253abd0be80bad6ea0267c0616e962e766f528
Pedro Angel <pavg_92@hotmail.com>
2022-12-15 22:33:07 -0600

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'proyecto001';
+  nombre = 'Pedro Velazquez';
+  edad = 24;
+  email = 'pavg.9286@gmail.com';
+  sueldos = [2321, 1213, 2320];
+  activo = true;
+  sitio = 'https://www.google.com';
+
+  esActivo() {
+    if(this.activo){
+      return 'Trabajador activo';
+    }else {
+      return 'Trabajador inactivo';
+    }
+  }
+
+  ultimosSueldos() {
+    let sum = 0;
+    for (let i = 0; i < this.sueldos.length; i++) {
+      sum += this.sueldos[i];
+    }
+
+    return sum;
+  }
 }
